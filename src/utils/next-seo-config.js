32177eda9ec6be0b9fb759916361d333861af9b3
headsmanc0de <headsmanc0de@gmail.com>
2023-11-27 21:47:50 +0200

    feat: add metatags

--- src/utils/next-seo-config.js
+++ src/utils/next-seo-config.js
@@ -1,15 +1,24 @@
 export const NEXT_SEO_DEFAULT = {
-	title: 'Придбай Steam Deck та Steam Dock за найкращою ціною та отримай повну підтримку в обслуговуванні приставки!',
-	titleTemplate: 'OnlyDeck',
+	title: 'Купуй Steam Deck і Steam Dock за найкращими цінами - OnlyDeck',
+	titleTemplate: 'OnlyDeck | Ваш надійний магазин Steam Deck в Україні',
 	canonical: 'https://www.onlydeck.shop/',
-	description: 'Придбай Steam Deck та Steam Dock за найкращою ціною та отримай повну підтримку в обслуговуванні приставки!',
+	description:
+		'OnlyDeck - ваш ідеальний магазин для придбання Steam Deck та Steam Dock. Вигідні ціни, ексклюзивні акції, широкий асортимент моделей, професійна підтримка та швидка доставка!',
 	openGraph: {
 		type: 'website',
 		locale: 'uk_UA',
 		url: 'https://www.onlydeck.shop/',
-		title: 'OnlyDeck',
+		title: 'OnlyDeck | Найкращий вибір Steam Deck та аксесуарів',
 		description:
-			'SteamDeck, SteamDeck 512, SteamDeck 256, SteamDeck 64, SteamDock, стім дек, стім дек 512, стім дек 256, стім дек 64, стім док, купити стім дек',
+			'В OnlyDeck ви знайдете найширший вибір Steam Deck моделей 512GB, 256GB, 64GB та Steam Dock. Вигідні ціни, унікальні пропозиції, гарантія якості та швидка доставка в Україні.',
 		siteName: 'OnlyDeck',
+		images: [
+			{
+				url: 'https://www.onlydeck.shop/images/og-image.jpg',
+				width: 800,
+				height: 600,
+				alt: 'OnlyDeck - Steam Deck',
+			},
+		],
 	},
 };
